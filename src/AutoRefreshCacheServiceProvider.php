1fdc260cf6319799ebb06fb956f3fffb13786e08
mirinfosys.khairul <mdkhairul773@gmail.com>
2025-08-12 09:46:20 +0600

    Update

--- src/AutoRefreshCacheServiceProvider.php
+++ src/AutoRefreshCacheServiceProvider.php
@@ -9,7 +9,10 @@ class AutoRefreshCacheServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__ . '/config/auto_refresh_cache.php', 'auto_refresh_cache');
+        $this->mergeConfigFrom(
+            __DIR__ . '/config/auto_refresh_cache.php',
+            'auto_refresh_cache'
+        );
     }
 
     public function boot()
@@ -17,7 +20,7 @@ class AutoRefreshCacheServiceProvider extends ServiceProvider
         // Publish config
         $this->publishes([
             __DIR__ . '/config/auto_refresh_cache.php' => config_path('auto_refresh_cache.php'),
-        ], 'config');
+        ], 'auto-refresh-cache-config');
 
         // Register command
         if ($this->app->runningInConsole()) {
